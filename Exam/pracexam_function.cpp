7515f34e8d5428dd93f5fdb4434f967d7663982b
Kunhua Huang <62396508+ecstasoy@users.noreply.github.com>
2023-12-16 03:36:04 +0800

    Delete Exam directory

--- Exam/pracexam_function.cpp
+++
@@ -1,7 +0,0 @@
-/*
-*  a) one-to-one, but not onto (wrong) not well defined
-*  b) onto, but not one-to-one
-*  c) neither one-to-one, nor onto
-*  d) one-to-one, but not onto (wrong) not well defined
-*  e) one-to-one, but not onto
-*/
